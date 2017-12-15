1541d0dc1141aa8f7857fa123b16a6df88676e2c
anxin_wang <506857724@qq.com>
2017-12-15 14:05:08 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@
 			<keep-alive>
 			    <router-view v-if="$route.meta.keepAlive"></router-view>
 			</keep-alive>
-    	</transition>
-    	<transition name="router-fade" mode="out-in">
+    </transition>
+    <transition name="router-fade" mode="out-in">
 			<router-view v-if="!$route.meta.keepAlive"></router-view>
 		</transition>
-		<svg-icon></svg-icon>	
+		<svg-icon></svg-icon>
     </div>
 </template>
 
